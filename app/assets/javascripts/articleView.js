a23326037018afd77a55865679cf70ea660183f4
theerickramer <theerickramer@gmail.com>
2014-11-15 16:52:45 -0500

    likes

---
+++ app/assets/javascripts/articleView.js
@@ -0,0 +1,2 @@
+var ArticleView = Backbone.View.extend({
+})
--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,4 +1,4 @@
-class LikeController < ApplicationController
+class LikesController < ApplicationController
 
 	def create
 		like = Like.create({liked: params[:liked], read: false, article_id: params[:article_id]})
