b0b2eaec0b4899bb8e88c0ce929f8bd09fb25d33
theerickramer <theerickramer@gmail.com>
2014-11-15 17:28:15 -0500

    articleView

--- app/assets/javascripts/articleView.js
+++ app/assets/javascripts/articleView.js
@@ -1,2 +1,33 @@
 var ArticleView = Backbone.View.extend({
+
+	template: _.template(),
+
+	initialize: function(){
+		this.render();
+	},
+
+	render: function(){
+		this.$el.html(this.template());
+	},
+
+	events {
+		'click .likeArticle' : 'like',
+		'click .dislikeArticle' : 'dislike'
+	},
+
+	like: function(){
+		$.post('/likes', {liked:true}).done(function(){
+			$.get('/articles').done(function(){
+			//instantiate new view
+			})
+		})
+	});
+
+	dislike: function(){
+		$.post('/likes', {liked:false}).done(function(){
+			$.get('/articles').done(function(){
+			//instantiate new view
+			})
+		})
+	});
 })
