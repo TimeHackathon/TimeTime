d80b6ab31b3aa850227a2e806d086d8d7c9c0a25
Iris Martinez <i.martinez8@umiami.edu>
2014-11-15 16:20:54 -0500

    delete categories table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141115210154) do
+ActiveRecord::Schema.define(version: 20141115211951) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,10 +22,6 @@ ActiveRecord::Schema.define(version: 20141115210154) do
     t.text   "content"
     t.text   "image"
     t.string "category"
-  end
-
-  create_table "categories", force: true do |t|
-    t.string "name"
   end
 
   create_table "likes", force: true do |t|
