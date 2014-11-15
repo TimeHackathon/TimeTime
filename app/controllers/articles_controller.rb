29e176eea2669e4960630d70f9b47941cd02cc76
conorhastings <hastings.conorm@gmail.com>
2014-11-15 15:54:31 -0500

    index logic

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -2,8 +2,9 @@ class ArticlesController < ApplicationController
 
 	def index
 		likes = Like.all.select(:article_id)
-		articles = Article.where()
+		articles = Article.where.not(id:likes).first
 
+		render json: articles
 
 	end
 
