881891ef041e822dfda0d1233685a44ca5f9e67d
theerickramer <theerickramer@gmail.com>
2014-11-16 08:14:27 -0500

    fixed put

--- app/assets/javascripts/articleListView.js
+++ app/assets/javascripts/articleListView.js
@@ -35,7 +35,7 @@ var ArticleListView = Backbone.View.extend({
 		if (this.options.read){
 			this.$el.empty().append('<li><button class="btn btn-sm close">X</button><h1>' + article.attributes.headline + '</h1><p>' + article.attributes.content + '</p></li>');
 		} else {
-			this.$el.empty().append('<li id="' + article.attributes.id + '"><h1>' + article.attributes.headline + '<button class="btn btn-sm close">X</button></h1><p>' + article.attributes.content + '</p><div class="bottom col-lg-offset-5"><button class="btn btn-md mark-read"> Read It</button><i class="fa fa-2x fa-facebook-square"></i><i class="fa fa-2x fa-twitter-square"></i></div></li>');
+			this.$el.empty().append('<li><h1>' + article.attributes.headline + '<button class="btn btn-sm close">X</button></h1><p>' + article.attributes.content + '</p><div class="bottom col-lg-offset-5"><button id="' + article.attributes.id + '" class="btn btn-md mark-read"> Read It</button><i class="fa fa-2x fa-facebook-square"></i><i class="fa fa-2x fa-twitter-square"></i></div></li>');
 		}
 	},
 	
@@ -44,7 +44,7 @@ var ArticleListView = Backbone.View.extend({
 	},
 
 	markRead: function(event){
-		$.ajax({url: '/likes/' + $(event.target).parent().prop('id'), type: 'PUT'});
+		$.ajax({url: '/likes/' + $(event.target).prop('id'), type: 'PUT'});
 		$('.likes').trigger('click');
 	}
 });
