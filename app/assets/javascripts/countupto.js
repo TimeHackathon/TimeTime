ed0df415d43c23dd8dfd954372f52287f1d3c435
conorhastings <hastings.conorm@gmail.com>
2014-11-16 07:53:22 -0500

    minutes counter

--- app/assets/javascripts/countupto.js
+++ app/assets/javascripts/countupto.js
@@ -9,9 +9,15 @@ for(var i = 0; i <= number; i++){
 }
 
 function loopTimeout(num){
-
+if(num.toString().length > 2){
+	var timeout = 100
+}else if(num.toString().length == 2){
+	var timeout = 70
+}else{
+	var timeout=35
+}
 	setTimeout(function(){
 		$('.timeRead').html(num)
-	},num*50)
+	},num*timeout)
 
 }
