08d6583734f192b175b5ff5526d95fce49f2bc94
conorhastings <hastings.conorm@gmail.com>
2014-11-16 00:07:41 -0500

    sdklsdkl

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -27,20 +27,60 @@ class ArticlesController < ApplicationController
 
 		else
 			likes = Like.all.select(:article_id)
-			articles = Article.where.not(id:likes).first
-			puts params[:count]
-			if(params[:count].to_i % 5 == 0)
-				articles = Ad.all.sample
+			categories = Article.where(id:likes)
+			like_stuff = {}
+			categories.each do |category|
+				Like.where(article_id:category.id)
+				if like_stuff.has_key?(category.category)
+					if(Like.find_by(article_id:category.id).liked == true)
+						like_stuff[category.category][:liked] += 1
+					else
+						like_stuff[category.category][:disliked] += 1
+					end
+
+					else
+						if(Like.find_by(article_id:category.id).liked == true)
+							like_stuff[category.category] = {liked:1 , disliked:0}
+						else
+							like_stuff[category.category] = {liked:0 , disliked:1}
+						end
+					end
+				end
+				like_percentages = {}
+				like_stuff.each do |k,v|
+					all = v[:liked]+v[:disliked]
+					# puts (v[:liked]/(v[:liked]+v[:disliked])).to_f
+					like_percentages[k] = v[:liked]/all.to_f
+				end
+				like_percentages = like_percentages.sort_by { |k, v| v}.reverse
+
+				articles = []
+				the_categories = []
+				like_percentages.each do |like|
+					articles.push(Article.where.not(id:likes).where(category:like[0]))
+					the_categories.push(like[0])
+				end
+
+				articles.push(Article.where.not(category:the_categories,id:likes))
+
+				articles.flatten!
+				puts articles.length
+
+				articles = articles[0]
+
+				puts params[:count]
+				if(params[:count].to_i % 5 == 0)
+					articles = Ad.all.sample
+				end
+
+
+
 			end
 
-
-
+			render json: articles
 		end
 
-		render json: articles
 	end
-
-end
 
 
 
