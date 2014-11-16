26aa5905e02f0421c2325d21e6cf0aa65127b80b
conorhastings <hastings.conorm@gmail.com>
2014-11-15 20:58:29 -0500

    get likes

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -5,6 +5,9 @@ class ArticlesController < ApplicationController
 		if(params[:category])
 			likes = Like.all.select(:article_id)
 			articles = Article.where.not(id:likes).where(category:params[:category]).first
+		elsif(params[:liked])
+			likes = Like.where(liked:true).select(:article_id)
+			articles = Article.where(id:likes)
 		else
 
 			likes = Like.all.select(:article_id)
